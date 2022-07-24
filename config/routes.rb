3eb078cb12f8d123360c4b3da908c3cd45d50546
grailwilly <grailjwi11y@gmail.com>
2022-07-24 10:55:40 +0800

    Add homepage and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "categories#index"
+  root "home#main"
   resources :categories do
     resources :tasks
   end
